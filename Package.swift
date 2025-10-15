4a82d71fae9aed3caf8f91264fcf673d69f64984
Berkin <bkabadayi@ziraatteknoloji.com>
2025-10-15 19:13:33 +0300

    package.swift update

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ZiraatAppAssistant",
-            url: "https://github.com/bkabadayi/ZiraatAppAssistant/releases/download/1.0.0/ZiraatAppAssistant-SPM.xcframework.zip",
+            url: "https://github.com/bkabadayi/ZiraatAppAssistant/releases/download/1.0.1/ZiraatAppAssistant-SPM.xcframework.zip",
             checksum: "a7813632e75040c9592b93688964327a1e2f4794bb70d917a9cf82c345e04a15"
         ),
     ]
